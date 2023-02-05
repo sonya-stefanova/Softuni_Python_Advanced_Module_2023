6e96bd97f3e6923f83b1784f4a395a197d5b06e8
Sonya Stefanova - Haralambieva <sonya_deanova@yahoo.com>
2023-02-05 14:35:27 +0200

    update

--- lists_and_stacks/05_hot_patato.py
+++ lists_and_stacks/05_hot_patato.py
@@ -1,17 +1,34 @@
-stack = []
-n = int(input())
-for _ in range(n):
-    command = input().split(" ")
-    num_command = command[0]
-    if command == "1":
-        number = int(command[1])
-        stack.append(number)
-    elif command == "2" and stack:
-        stack.pop()
-    elif command == "3" and stack:
-        print(max(stack))
-    elif command == "4" and stack:
-        print(min(stack))
+# from _collections import deque
+#
+# name_of_players = input().split(' ')
+# step_of_hot_potato = int(input())
+#
+# players_deque = deque(name_of_players)
+# counter = 0
+#
+# while len(players_deque) > 1:
+#     counter += 1
+#     current_name_of_player = players_deque.popleft()
+#
+#     if counter == step_of_hot_potato:
+#         print(f'Removed {current_name_of_player}')
+#         counter = 0
+#     else:
+#         players_deque.append(current_name_of_player)
+#
+# print(f'Last is {players_deque[0]}')
 
-stack.reverse()
-print(stack)
+from _collections import deque
+
+name_of_players = input().split(' ')
+step_of_hot_potato = int(input())
+players_deque = deque(name_of_players)
+
+
+while len(players_deque) > 1:
+    for _ in range(step_of_hot_potato - 1):
+        players_deque.append(players_deque.popleft())
+
+    print(f'Removed {players_deque.popleft()}')
+
+print(f'Last is {players_deque.pop()}')
